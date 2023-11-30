6870263f6d36d8aaaf21b38109c9da904f32d950
anirudhram37 <anirudhram37@gmail.com>
2023-11-29 22:16:13 -0700

    test

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request
+import psycopg2
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+def db_connect():
+    # have to change parameters to the configurations for your database
+    conn  = psycopg2.connect(database='ecommerce_db', host='localhost', user='postgres', password='password', port='5432')
+    return conn
 
-db=SQLAlchemy(app)
 
 @app.route('/')
 def index():
---
+++ init_db.py
@@ -0,0 +1,14 @@
+import psycopg2
+
+conn  = psycopg2.connect(database='ecommerce_db', host='localhost', user='postgres', password='password', port='5432')
+cur = conn.cursor()
+
+cur.execute("INSERT INTO Product Values ( 'B002PD61Z5', 'Macbook', 1500, 'Computers', 'https://macbook.jpg', 'https://macbook', 'Apple Macbook Pro', 'China', 4.0599999904632568, 9000);")
+
+conn.commit()
+cur.close()
+conn.close()
+
+
+
+
