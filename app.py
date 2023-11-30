eab1d796a95a920b575f01accbca668ec0428cde
Dhruv Samdani <dsamdani@asu.edu>
2023-11-29 22:33:14 -0700

    added test.html (remove later) auto generated for rough idea

--- app.py
+++ app.py
@@ -6,13 +6,18 @@ app = Flask(__name__)
 
 def db_connect():
     # have to change parameters to the configurations for your database
-    conn  = psycopg2.connect(database='ecommerce_db', host='localhost', user='postgres', password='password', port='5432')
+    conn  = psycopg2.connect(database='dbproject', host='localhost', user='postgres', password='password', port='5432')
     return conn
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/connection")
+def page():
+    conn = db_connect()
+    return {'c' : str(conn)}
 
 if __name__ == '__main__':
     app.run(debug=True)
